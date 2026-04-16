737b463d623fee5ae88c4ffbd2c196a399502e72
Nicole Hurey <251768491+theosintvault@users.noreply.github.com>
2026-04-16 10:00:22 -0400

    Update setup.py with long description and version change
    Updated version number and added long description from README.md.

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="osintvault",
-    version="0.1.2",
+    version="0.1.10",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     entry_points={
         'console_scripts': [
             'osintvault=cli.main:main',
